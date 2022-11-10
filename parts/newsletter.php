431a2718e79ac00dce8836ab4d2c92e8239c82ea
Benjamin <113176528+cosmospalace@users.noreply.github.com>
2022-11-10 10:04:14 +0100

    Add files via upload

---
+++ parts/newsletter.php
@@ -0,0 +1,5 @@
+<form class="newsletter-form">
+    <p>Toute l'actu de la culture dans votre boîte mail</p>
+    <input type="email" name="email" placeholder="E-mail">
+    <input type="submit" value="Je m'abonne">
+</form>
